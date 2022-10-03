8fb3e413051e4d226f74e2819dcb0256c9f8f821
Alex <as1857@ship.edu>
2022-10-03 13:11:09 -0400

    Event Handling

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const token = process.env.DISCORD_TOKEN
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] })
 
-// The commands
+// Command Handling
 client.commands = new Collection()
 const commandsPath = path.join(__dirname, 'commands')
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
@@ -22,25 +22,19 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command)
 }
 
-// When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
-})
+// Event Handling
+const eventsPath = path.join(__dirname, 'events')
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'))
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return
-
-	const command = interaction.client.commands.get(interaction.commandName)
-
-	if(!command) return
-
-	try {
-		await command.execute(interaction)
-	} catch (error) {
-		console.error(error)
-		await interaction.reply({ content: 'There was an error while executing this command', ephemeral: true})
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file)
+	const event = require(filePath)
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args))
+	} else {
+		client.on(event.name, (...args) => event.execute(...args))
 	}
-})
+}
 
 // Login to Discord with your client's token
 client.login(token)
