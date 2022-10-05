9494358195accb4bb4b5205c0508ea33938329ba
Alex <as1857@ship.edu>
2022-10-05 14:03:27 -0400

    cleaned up reschedule

--- commands/reschedule.js
+++ commands/reschedule.js
@@ -4,7 +4,7 @@ const { Match } = require('../models/match.js')
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('schedule')
+        .setName('reschedule')
         .setDescription('Schedules a time for a match')
         .addIntegerOption(option =>
             option.setName('match_id')
@@ -24,7 +24,6 @@ module.exports = {
         currDate = DateTime.utc()
         // TODO: see if i can make it so osu players can just put SAT or SUN.
         // TODO: error handling for incorrect input on time/support just HH time
-        const year = currDate.year
         month = currDate.month
         const day = parseInt(interaction.options.getString('day'))
         // This assumes that the player is scheduling for the next month
@@ -36,8 +35,8 @@ module.exports = {
         // We can assume that they entered HH:MM
         const hour = parseInt(time[0])
         const minute = parseInt(time[1])
-        const matchTime = Math.trunc(DateTime.local(year, month, day, hour, minute,{ zone: "utc" }))
-
+        const matchTime = Math.trunc(DateTime.fromObject({ month: month, day: day, hour: hour, minute: minute } , { zone: "utc" }))
+        
         // mongoDB and js store Dates as milliseconds
         const match = await Match.findOneAndUpdate({ _id: matchID }, { time: matchTime })
 
