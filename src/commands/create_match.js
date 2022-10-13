6f5e0c8aba49259b16c83777fa5b83f5363b1ba6
Alex Schreffler <aschreff53@icloud.com>
2022-10-13 10:46:32 -0400

    cleaned up format

--- src/commands/create_match.js
+++ src/commands/create_match.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder } = require('discord.js')
 const { DateTime } = require('luxon')
 const { Match } = require('../models/match.js')
 
