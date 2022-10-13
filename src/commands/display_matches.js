5bb9adc3736e57bfa5b52b3b7513f349814c1ffb
Alex Schreffler <aschreff53@icloud.com>
2022-10-13 10:47:11 -0400

    started on the display match command

---
+++ src/commands/display_matches.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('discord.js');
+const { DateTime } = require('luxon')
+const { Match } = require('../models/match.js')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('display_matches')
+        .setDescription('Displays matches')
+        .addBooleanOption(option => 
+            option.setName('show_past')
+                .setDescription('True if you want to see past matches (Before current time')),
+
+    async execute(interaction) {
+        var currTime = DateTime.utc()
+        
+        // if we want to see past matches
+        if (interaction.options.getBoolean('show_past')) {
+            currTime = 0
+        }
+        const matches = await Match.find({ time: { $gte: currTime } }).exec()
+        var output = "The upcoming matches are:\n"
+        matches.forEach (match =>
+            output += `Match ${match._id} between ${match.team1} and ${match.team2} : <t:${match.time / 1000}:R>\n`
+        )
+        await interaction.reply(output)
+    }
+}
