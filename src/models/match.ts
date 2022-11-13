a3657286eef6d89e9a559e06ccd4a7d44911fe1a
Alex Schreffler <aschreff53@icloud.com>
2022-11-13 18:30:10 -0500

    database stuff

--- src/models/match.ts
+++ src/models/match.ts
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-// Store match date as number for simplification and use of epoch time
 const matchSchema = mongoose.Schema({
     _id: { type: Number, required: true },
     // team1: { type: mongoose.Types.ObjectId, required: true, ref: 'Team' },
     // team2: { type: mongoose.Types.ObjectId, required: true, ref: 'Team' },
     team1: { type: String, required: true },
     team2: { type: String, required: true },
+    // Store match date as number for simplification and use of epoch time
     time: { type: Number, required: true },
 });
 
---
+++ src/models/player.ts
@@ -0,0 +1,14 @@
+import { Schema, model } from 'mongoose';
+
+const playerSchema = new Schema({
+    _id: { type: Number, required: true },
+    discord: { type: Number, required: true },
+    rank: { type: Number, required: true },
+    badges: { type: Number, required: true },
+    // number relative to UTC
+    timezone: { type: Number, required: true },
+});
+
+const Player = model('Player', playerSchema);
+
+export { Player }
