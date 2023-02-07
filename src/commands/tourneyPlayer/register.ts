d463c61488e6028c6d5cc481335da52970f7921c
Alex Schreffler <aschreff53@icloud.com>
2023-02-07 14:29:42 -0500

    worked on team aspect of register

--- src/commands/tourneyPlayer/register.ts
+++ src/commands/tourneyPlayer/register.ts
@@ -1,71 +1,185 @@
 const { SlashCommandBuilder } = require('discord.js');
-import { ActionRowBuilder, ButtonBuilder, ChatInputCommandInteraction, ButtonStyle, Events, ModalBuilder, TextInputBuilder, TextInputStyle, ModalActionRowComponentBuilder, ModalSubmitInteraction, Collector, EmbedBuilder } from 'discord.js';
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ChatInputCommandInteraction,
+    ButtonStyle,
+    Events,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle,
+    ModalActionRowComponentBuilder,
+    ModalSubmitInteraction,
+    Collector,
+    EmbedBuilder,
+    ComponentType,
+    ButtonInteraction,
+    ActionRow,
+    Message,
+    time,
+} from 'discord.js';
+
+import { CollectorUtils } from 'discord.js-collector-utils';
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('register')
-        .setDescription('Register for the tournament'),
-    
+    data: new SlashCommandBuilder().setName('register').setDescription('Register for the tournament'),
+
     async execute(interaction: ChatInputCommandInteraction) {
         // TODO: logic to determine if this tourney is team/1v1 (max team size). Also add special case for draft
+        // TODO: make expires ephemeral
 
-        // Logic for team tournaments
-
-        const teamOrFARow = new ActionRowBuilder<ButtonBuilder>()
-            .addComponents(
-                new ButtonBuilder()
-                    .setCustomId('team')
-                    .setLabel('Team')
-                    .setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-                    .setCustomId('free_agent')
-                    .setLabel('Free Agent')
-                    .setStyle(ButtonStyle.Primary),
-                new ButtonBuilder()
-                    .setCustomId('solo')
-                    .setLabel('Solo Tournament')
-                    .setStyle(ButtonStyle.Primary),
-            );
-
-        await interaction.reply({ content: 'Are you signing up as a team, as a free agent, or for a solo tournament?', components: [teamOrFARow], ephemeral: true});
-
-        const teamOrFACollector = interaction.channel!.createMessageComponentCollector({ time: 15000 })
-        teamOrFACollector.on('collect', async teamOrFAInteraction => {
-            if (teamOrFAInteraction.customId == 'team') {
-                // Create a modal for discord that has a text input for team name and timezone
-                const teamInfoModal = new ModalBuilder()
-                    .setCustomId('team_info')
-                    .setTitle('Initial Team Information')
-
-                const teamNameInput = new TextInputBuilder()
-                    .setCustomId('team_name')
-                    .setPlaceholder('Team Name')
-                    .setStyle(TextInputStyle.Short)
-                    .setLabel('Team Name');
-                const timezoneInput = new TextInputBuilder()
-                    .setCustomId('timezone')
-                    .setPlaceholder('In UTC-/+ format')
-                    .setStyle(TextInputStyle.Short)
-                    .setLabel('Timezone');
-
-                const teamNameRow = new ActionRowBuilder<ModalActionRowComponentBuilder>()
-                    .addComponents(teamNameInput);
-                const timezoneRow = new ActionRowBuilder<ModalActionRowComponentBuilder>()
-                    .addComponents(timezoneInput);
-
-                teamInfoModal.addComponents(teamNameRow, timezoneRow);
-
-                await teamOrFAInteraction.showModal(teamInfoModal);
-
-            } else if (teamOrFAInteraction.customId == 'free_agent') {
-                // grab timezone, comments, etc.
-            } else if (teamOrFAInteraction.customId == 'solo') {
-                // put in DB then done.
-            }
-        })
-
-        teamOrFACollector.on('end', collected => {
-            console.log(`Collected ${collected.size} interactions.`);
+        await interaction.reply({
+            content: 'You will now be walked through the registration process',
+            ephemeral: true,
         });
+        // Take what format they want to register as
+        let formatPrompt = await interaction.followUp({
+            content: 'Are you registering as a team or as a free agent?',
+            components: [
+                {
+                    type: ComponentType.ActionRow,
+                    components: [
+                        {
+                            type: ComponentType.Button,
+                            customId: 'team',
+                            label: 'Team',
+                            style: ButtonStyle.Primary,
+                        },
+                        {
+                            type: ComponentType.Button,
+                            customId: 'freeAgent',
+                            label: 'Free Agent',
+                            style: ButtonStyle.Primary,
+                        },
+                    ],
+                },
+            ],
+            ephemeral: true,
+        });
+
+        let formatResult = await CollectorUtils.collectByButton(
+            formatPrompt,
+            // Retrieve Result
+            async (buttonInteraction) => {
+                switch (buttonInteraction.customId) {
+                    case 'team':
+                        return { intr: buttonInteraction, value: 'Team' };
+                    case 'freeAgent':
+                        return { intr: buttonInteraction, value: 'Free Agent' };
+                    default:
+                        return;
+                }
+            },
+            // Options
+            {
+                time: 10000,
+                reset: true,
+                target: interaction.user,
+                stopFilter: (message) => message.content.toLowerCase() === 'stop',
+                onExpire: async () => {
+                    await interaction.channel!.send({ content: 'Too slow! Try being more decisive next time.' });
+                },
+            }
+        );
+
+        if (formatResult === undefined) {
+            return;
+        }
+
+        // Based on format, take team info or free agent info
+        switch (formatResult.value) {
+            case 'Team':
+                // Build a modal to take team info (team name and timezone)
+                await formatResult.intr.reply({ content: 'You have selected team', ephemeral: true });
+
+                let teamInfoPrompt = await formatResult.intr.followUp({
+                    content: 'Please input your team information.',
+                    components: [
+                        {
+                            type: ComponentType.ActionRow,
+                            components: [
+                                {
+                                    type: ComponentType.Button,
+                                    customId: 'enter_response',
+                                    emoji: '⌨️',
+                                    label: 'Enter Response',
+                                    style: ButtonStyle.Primary,
+                                },
+                            ],
+                        },
+                    ],
+                    ephemeral: true,
+                });
+
+                let teamInfoResult = await CollectorUtils.collectByModal(
+                    teamInfoPrompt,
+                    // Retrieve Result
+                    new ModalBuilder({
+                        customId: 'teamInfoModal',
+                        title: 'Team Information',
+                        components: [
+                            {
+                                type: ComponentType.ActionRow,
+                                components: [
+                                    {
+                                        type: ComponentType.TextInput,
+                                        customId: 'teamName',
+                                        label: 'Team Name',
+                                        style: TextInputStyle.Short,
+                                        required: true,
+                                    },
+                                ],
+                            },
+                            {
+                                type: ComponentType.ActionRow,
+                                components: [
+                                    {
+                                        type: ComponentType.TextInput,
+                                        customId: 'timezone',
+                                        label: 'Timezone',
+                                        style: TextInputStyle.Short,
+                                        required: true,
+                                    },
+                                ],
+                            },
+                        ],
+                    }),
+
+                    async (modalSubmitInteraction) => {
+                        let teamName = modalSubmitInteraction.components[0].components[0];
+                        let timezone = modalSubmitInteraction.components[1].components[0];
+                        if (teamName.type !== ComponentType.TextInput || timezone.type !== ComponentType.TextInput) {
+                            return;
+                        }
+                        // TODO: logic for determining validity of team name / timezone
+                        return {
+                            intr: modalSubmitInteraction,
+                            value: { teamName: teamName.value, timezone: timezone.value },
+                        };
+                    },
+                    // Options
+                    {
+                        time: 10000,
+                        reset: true,
+                        target: interaction.user,
+                        stopFilter: (message) => message.content.toLowerCase() === 'stop',
+                        onExpire: async () => {
+                            await interaction.channel!.send('Too slow! Try being more decisive next time.');
+                        },
+                    }
+                );
+                if (teamInfoResult == undefined) {
+                    return;
+                }
+
+                await teamInfoResult.intr.reply({
+                    content: ` Your team name is ${teamInfoResult.value.teamName} and your time zone is ${teamInfoResult.value.timezone}`,
+                    ephemeral: true,
+                });
+            case 'Free Agent':
+                return;
+            default:
+                return;
+        }
     },
 };
