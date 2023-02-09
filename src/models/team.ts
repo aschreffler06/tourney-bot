d31cf05f81c04a730d20065d4b0d195630033498
Alex Schreffler <aschreff53@icloud.com>
2023-02-09 11:36:47 -0500

    continued work on register

--- src/models/team.ts
+++ src/models/team.ts
@@ -4,11 +4,13 @@ import { IPlayer, playerSchema } from './index';
 type ITeam = {
     name: string;
     players: IPlayer[];
+    timezone: string;
 };
 
 const teamSchema = new Schema<ITeam>({
     name: { type: String, required: true },
     players: { type: [playerSchema], required: true },
+    timezone: { type: String, required: true },
 });
 
 const Team = model<ITeam>('Team', teamSchema);
